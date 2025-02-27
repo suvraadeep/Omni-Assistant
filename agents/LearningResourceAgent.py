3d3fada90acb8b26ba8ceaf2ad3abe58625f79c0
Suvradeep <suvradeep@iitg.ac.in>
2025-02-27 21:18:55 +0530

    agents added

---
+++ agents/LearningResourceAgent.py
@@ -0,0 +1,43 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_community.tools import DuckDuckGoSearchResults #searching tools
+from langchain.agents import create_tool_calling_agent
+from langchain.agents import AgentExecutor
+
+class LearningResourceAgent:
+    def __init__(self, prompt):
+        self.model = ChatGroq(model="llama-3.3-70b-versatile")
+        self.prompt = prompt
+        self.tools = [DuckDuckGoSearchResults()]
+
+    def TutorialAgent(self, user_input):
+        agent = create_tool_calling_agent(self.model, self.tools, self.prompt)
+        agent_executor = AgentExecutor(agent=agent, tools=self.tools, verbose=True)
+        response = agent_executor.invoke({"input": user_input})
+        path = save_file(str(response.get('output')).replace("```markdown", "").strip(), 'Tutorial')
+        print(f"Tutorial saved to {path}")
+        return path
+
+    def QueryBot(self, user_input):
+        print("\nStarting the Q&A session. Type 'exit' to end the session.\n")
+        record_QA_session = []
+        record_QA_session.append('User Query: %s \n' % user_input)
+        self.prompt.append(HumanMessage(content=user_input))
+        while True:
+            self.prompt = trim_conversation(self.prompt)
+            response = self.model.invoke(self.prompt)
+            record_QA_session.append('\nExpert Response: %s \n' % response.content)
+            
+            self.prompt.append(AIMessage(content=response.content))
+            print('*' * 50 + 'AGENT' + '*' * 50)
+            print("\nEXPERT AGENT RESPONSE:", response.content)
+            
+            print('*' * 50 + 'USER' + '*' * 50)
+            user_input = input("\nYOUR QUERY: ")
+            record_QA_session.append('\nUser Query: %s \n' % response.content)
+            self.prompt.append(HumanMessage(content=user_input))
+
+            if user_input.lower() == "exit":
+                print("Ending the chat session.")
+                path = save_file(''.join(record_QA_session),'Q&A_Doubt_Session')
+                print(f"Q&A Session saved to {path}")
+                return path
